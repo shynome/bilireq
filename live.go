3e06a10703dabe49a059cb5947785a6f24ac1b30
shynome <shynome@gmail.com>
2026-03-02 00:04:57 +0800

    时间移到单独的文件中

--- live.go
+++ live.go
@@ -2,8 +2,6 @@ package bilireq
 
 import (
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/shynome/err0"
 	"github.com/shynome/err0/try"
@@ -52,27 +50,6 @@ func (api *Client) LiveFeedAll() (feeds []LiveInfo, err error) {
 		}
 	}
 	return
-}
-
-type BilibiliTime time.Time
-
-const BilibiliTimeLayout = "2006-01-02 15:04:05"
-const BilibiliTimeEmpty = "0000-00-00 00:00:00"
-
-var BilibiliTimeLoc = try.To1(time.LoadLocation("Asia/Shanghai"))
-
-func (c *BilibiliTime) UnmarshalJSON(b []byte) error {
-	value := strings.Trim(string(b), `"`) //get rid of "
-	if value == "" || value == "null" || value == BilibiliTimeEmpty {
-		return nil
-	}
-
-	t, err := time.ParseInLocation(BilibiliTimeLayout, value, BilibiliTimeLoc) //parse time
-	if err != nil {
-		return err
-	}
-	*c = BilibiliTime(t) //set result using the pointer
-	return nil
 }
 
 type LiveStatus int
